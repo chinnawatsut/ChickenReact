3baa0a92cb7a56f73733d026b33a50e0f83b9cc3
Chinnawat K <chinnawatsut@gmail.com>
2017-07-10 18:00:57 +0700

    inline compare to ng-show

--- my-first-app/src/components/Welcome.js
+++ my-first-app/src/components/Welcome.js
@@ -9,18 +9,29 @@ export default class Welcome extends Component {
     this.handleClick = this.handleClick.bind(this);
    }
 
-    handleClick() {
-    this.setState(prevState => ({
-      name: "kera"
-    }));
-  }
+    handleClick = () => {
+      console.log("handle Click");          
+      console.log(this);
+            this.setState(prevState => ({
+                  name: "kera"
+            }));
+      }
+
+      passingClick = () => {
+            console.log("Passing Click");            
+            console.log(this);
+      }
+
    render(){
       
       return (
          <div>
                <h1 onClick={this.handleClick}>Welcome Mr. {this.state.name} </h1>
+               <button onClick={e => this.passingClick(e)}>Click Me</button>
+               {this.state.name === "kera" && 
+                  <h2>Name Changed</h2>
+               }
          </div>
-      
       );
    }
 }
